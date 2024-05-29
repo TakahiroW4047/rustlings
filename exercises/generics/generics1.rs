a553deabc9fd3e3d810c00c35db4fb6c33042bd5
Takahiro Watanabe <tak@macbookpro.local>
2024-05-28 20:06:01 -0700

    Complete generics and clippy lessons.

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -6,9 +6,11 @@
 // Execute `rustlings hint generics1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
-    shopping_list.push("milk");
+    let mut shopping_list = Vec::new();
+    add_to_list(&mut shopping_list, "milk");
+}
+
+fn add_to_list<T>(item_list: &mut Vec<T>, item: T) {
+    item_list.push(item);
 }
